b222941d7ed822e9f6bacf969e5ea3f7ec5af55f
JoshyBrown <suraj.joshua@gmail.com>
2018-02-18 14:53:09 +0100

    config spring + WEB-INF

---
+++ WemanityDjSpring/src/main/java/org/perso/spring/config/ApplicationConfig.java
@@ -0,0 +1,64 @@
+package org.perso.spring.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages= {"org.perso.spring"})
+@EnableTransactionManagement
+public class ApplicationConfig {
+
+	@Autowired
+	private Environment environment;
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty("jdbc.driverClass"));
+		dataSource.setUrl(environment.getProperty("jdbc.url"));
+		dataSource.setUsername(environment.getProperty("jdbc.username"));
+		dataSource.setPassword(environment.getProperty("jdbc.password"));
+		return dataSource;
+	}
+	
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+		jpaTransactionManager.setEntityManagerFactory(emf);
+		return jpaTransactionManager;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdaptater() {
+		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+		jpaVendorAdapter.setDatabase(Database.MYSQL);
+		jpaVendorAdapter.setShowSql(true);
+		return jpaVendorAdaptater();
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(dataSource());
+		emf.setJpaVendorAdapter(jpaVendorAdaptater());
+		emf.setPackagesToScan("org.perso.spring.model");
+		Properties jpaProperties = new Properties();
+		emf.setJpaProperties(jpaProperties);
+		return emf;
+	}
+	
+}
---
+++ WemanityDjSpring/src/main/java/org/perso/spring/config/WebConfig.java
@@ -0,0 +1,38 @@
+package org.perso.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages= {"org.formation.spring"})
+@Import({ApplicationConfig.class})
+@PropertySource({"classpath:application.properties"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp ");
+		return resolver;
+	}
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+}
